d576ac1e701925fe34d10948717f472321429212
Arkadiusz Gil <arkadiusz.gil@erlang-solutions.com>
2017-06-28 11:01:14 +0200

    Add docs

--- lib/phx_load_gen.ex
+++ lib/phx_load_gen.ex
@@ -1,6 +1,21 @@
 defmodule PhxLoadGen do
-  @moduledoc false
+  @moduledoc """
+  Generate load on Phoenix channels!
+  """
 
+  @doc """
+  Start batch of Phoenix clients
+
+  * `url` is a websocket Phoenix endpoint URL (usually ws://HOST:PORT/socket/websocket)
+  * `start_id` and `end_id` is a range of identifiers assigned to clients
+  * `topic` the topic clients should join (when used with https://github.com/arkgil/phoenix_chat_example
+    it should be `"rooms:lobby"`)
+  * `send_interval` how often clients should send messages
+  * `send_repeats` how many messages will be send
+
+  On success it'll return `{:ok, pid}`. Returned pid can be passed to `stop/1`
+  to stop the clients.
+  """
   def start(url, start_id, end_id, topic, send_interval, send_repeats) do
     case Supervisor.start_child(PhxLoadGen.Supervisor, []) do
       {:ok, pid} ->
@@ -8,6 +23,7 @@ defmodule PhxLoadGen do
           {:ok, _} = Supervisor.start_child(pid,
             [url, id, topic, send_interval, send_repeats])
         end
+        {:ok, pid}
       e ->
         e
     end
